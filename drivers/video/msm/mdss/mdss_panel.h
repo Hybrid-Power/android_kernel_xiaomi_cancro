1e28c2b11698e759b58868ffd6535c64d2b1117b
Apr20th <1131141128@qq.com>
2016-01-02 17:38:36 +0800

    Revert "*WIP"
    This reverts commit 80008496f1b1f40103f1ad81dd0b043cda4c5215.

--- drivers/video/msm/mdss/mdss_panel.h
+++ drivers/video/msm/mdss/mdss_panel.h
@@ -270,8 +270,6 @@ struct fbc_panel_info {
 	u32 lossy_mode_idx;
 };
 
-struct mdss_livedisplay_ctx;
-
 struct mdss_panel_info {
 	u32 xres;
 	u32 yres;
@@ -321,7 +319,6 @@ struct mdss_panel_info {
 	struct mipi_panel_info mipi;
 	struct lvds_panel_info lvds;
 	struct edp_panel_info edp;
-    struct mdss_livedisplay_ctx *livedisplay;
 };
 
 struct mdss_panel_data {
